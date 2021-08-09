9c86cc0f2e11bcea7f781b05ddb962dee5a43123
prajjwalpathak <prajjwalpathak.asm@gmail.com>
2021-08-10 01:26:44 +0530

    Add success.html and failure.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const request = require("request");
+const https = require("https");
 const path = require("path");
 
 const app = express();
@@ -24,18 +25,42 @@ app.post("/", (req, res) => {
                     LNAME: lName
                 },
                 email_address: email,
-                status: "subscribed",
-            },
-        ],
+                status: "subscribed"
+            }
+        ]
     };
 
     const jsonData = JSON.stringify(data);
+
+    url = 'https://us5.api.mailchimp.com/3.0/lists/6e3656cb28';
+    options = {
+        method: "POST",
+        auth: "prajjwalpathak:bce8d47555d9881922490f23b44bf1f9-us5"
+    };
+
+    const request = https.request(url, options, (response)=> {
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+    });
+
+    request.write(jsonData);
+    request.end();
+
+
+});
+
+app.post("/failure", (req, res)=> {
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
     console.log("Server running at port 3000");
 });
 
-//API Key b73fc773d50ad60e3db9eab72148892a-us5
+//API Key bce8d47555d9881922490f23b44bf1f9-us5
 
 //List ID 6e3656cb28
